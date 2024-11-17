7ea61c4c28ad365aaf505a7742bfff93d150ecac
Ezepue <ebukaezepue@gmail.com>
2024-11-17 23:38:50 +0100

    fixed Model

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # Define Task Model
-class Task(db.model):
+class Task(db.Model):
     __tablename__ ='tasks'
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(100), nullable=False)
